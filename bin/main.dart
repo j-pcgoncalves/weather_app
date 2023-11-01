e48bdd49f3bca5c0c39ac9b145c823ebc17f2734
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-11-01 20:56:54 +0000

    read response data

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,13 @@
-void main(List<String> arguments) {
+import 'weather_api_client.dart';
+
+void main(List<String> arguments) async {
   if (arguments.length != 1) {
     print("Syntax: dart bin/main.dart <city>");
     return;
   }
 
   final city = arguments.first;
-  print(city);
+  final api = WeatherApiClient();
+  final locationWeather = await api.getLocationInfo(city);
+  print(locationWeather);
 }
--- bin/weather_api_client.dart
+++ bin/weather_api_client.dart
@@ -1,10 +1,29 @@
+import "dart:convert";
+
 import "package:http/http.dart" as http;
 
-class WeatherApiClient {
-  static const baseUrl = "https://www.metaweather.com/api";
+import "secret_config.dart";
 
-  Future<int> getLocationId(String city) async {
-    final locationUrl = Uri.parse("$baseUrl/location/search/?query=$city");
-    final locationResponse = await http.get(locationUrl);
+class WeatherApiClient {
+  SecretConfig _API_SECRET = SecretConfig();
+  static const baseUrl =
+      "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather";
+
+  Future<Map<String, dynamic>> getLocationInfo(String city) async {
+    final locationUrl = Uri.parse("$baseUrl?city=$city");
+    final locationResponse = await http.get(
+      locationUrl,
+      headers: {
+        "X-RapidAPI-Key": _API_SECRET.key,
+        "X-RapidAPI-Host": _API_SECRET.host,
+      },
+    );
+
+    if (locationResponse.statusCode != 200) {
+      throw Exception("Error getting locationId for city: $city");
+    }
+
+    final locationJson = jsonDecode(locationResponse.body);
+    return locationJson;
   }
 }
